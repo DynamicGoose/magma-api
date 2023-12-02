0c09b25263dafd9124cd860b3fb72773e70226ed
Géza Ahsendorf <gezaahs@gmail.com>
2023-12-02 02:08:29 +0100

    documentation for magma_app

--- crates/magma_app/src/lib.rs
+++ crates/magma_app/src/lib.rs
@@ -5,6 +5,7 @@ pub mod module;
 
 type Systems<'a> = (Vec<&'a dyn Fn(&World)>, Vec<&'a dyn Fn(&mut World)>);
 
+/// The `App` struct holds all the apps data and defines the necessary functions and methods to operate on it.
 #[derive(Default)]
 pub struct App<'a> {
     pub world: World,
@@ -13,14 +14,50 @@ pub struct App<'a> {
 }
 
 impl<'a> App<'a> {
+    /// Create a new `App`
     pub fn new() -> Self {
         Self::default()
     }
 
+    /**
+    Add a `Module` to the `App`.
+    ```
+    use magma_app::{module::Module, App};
+
+    let mut app = App::new();
+    app.add_module(ExampleModule);
+
+    struct ExampleModule;
+
+    impl Module for ExampleModule {
+        fn setup(&self, app: &mut App) {
+            // Setup the module
+            // E.g. register components to the World or add resources
+        }
+    }
+    ```
+    */
     pub fn add_module(&mut self, module: impl Module) {
         module.setup(self);
     }
 
+    /**
+    Add systems to the `App`'s `World`. Systems must take either an immutable or a mutable reference to `World`.
+    ```
+    use magma_app::{App, SystemType, World};
+
+    let mut app = App::new();
+    app.add_systems(SystemType::Startup, (vec![&example_system_ref], vec![&example_system_mut]));
+
+    fn example_system_ref(_world: &World) {
+        // Do something
+    }
+
+    fn example_system_mut(_world: &mut World) {
+        // E.g. change something in the World
+    }
+    ```
+    */
     pub fn add_systems(&mut self, systemtype: SystemType, systems: Systems<'a>) {
         match systemtype {
             SystemType::Startup => {
@@ -46,6 +83,7 @@ impl<'a> App<'a> {
         }
     }
 
+    /// Run the application
     pub fn run(&mut self, update_condition: &dyn Fn(&World) -> bool) {
         self.world.startup(
             self.startup_systems.0.clone(),
@@ -59,6 +97,7 @@ impl<'a> App<'a> {
     }
 }
 
+/// Used to specify if systems should be added to the `startup` or `update` loop of the `World`
 pub enum SystemType {
     Startup,
     Update,
--- crates/magma_app/src/module.rs
+++ crates/magma_app/src/module.rs
@@ -1,5 +1,6 @@
 use crate::App;
 
+/// Must be implemented on your own modules for them to work
 pub trait Module {
     fn setup(&self, app: &mut App);
 }
