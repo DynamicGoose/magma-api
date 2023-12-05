773f913d6d2e568537355c7c0a47cc5e06accaf9
DynamicGoose <gezaahs@gmail.com>
2023-12-05 11:10:57 +0100

    implement winit event_loop

--- crates/magma_winit/src/lib.rs
+++ crates/magma_winit/src/lib.rs
@@ -2,17 +2,12 @@ use magma_app::{module::Module, App};
 use winit::{
     event::{Event, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
-    window::{Window, WindowBuilder},
+    window::WindowBuilder,
 };
 
 pub struct WinitModule;
 
-impl WinitModule {
-    pub fn create_window() {
-        let event_loop = EventLoop::new().unwrap();
-        Window::new(&event_loop).unwrap();
-    }
-}
+impl WinitModule {}
 
 impl Module for WinitModule {
     fn setup(&self, app: &mut magma_app::App) {
