02ced67575247f4f44a979aed371300dca256931
Géza Ahsendorf <gezaahs@gmail.com>
2023-12-06 22:57:05 +0100

    add ui crate

---
+++ crates/magma_ui/README.md
@@ -0,0 +1,1 @@
+# magma_ui
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 pub use magma_app;
 pub use magma_audio;
+pub use magma_ui;
 pub use magma_winit;
 
 /// The Magma-API crate is a container crate for all the featrues that it provides.
