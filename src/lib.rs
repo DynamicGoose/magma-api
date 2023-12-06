10723187fd461ca47c5f9dbe65409c028023e6bb
Géza Ahsendorf <gezaahs@gmail.com>
2023-12-06 23:45:55 +0100

    improve api documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,8 @@
+//! The Magma-API crate is a container crate to combine all the featrues that are in seperate crates.
 pub use magma_app;
 pub use magma_audio;
 pub use magma_ui;
 pub use magma_winit;
-
-/// The Magma-API crate is a container crate for all the featrues that it provides.
 
 #[cfg(test)]
 mod tests {}
