31bf628f248e73c61a4b8f2288e82142f7eb057b
Géza Ahsendorf <gezaahs@gmail.com>
2023-12-10 23:03:50 +0100

    add `DefaultModules`

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,20 @@
 pub use magma_app;
 pub use magma_audio;
 pub use magma_ui;
+use magma_ui::UIModule;
 pub use magma_winit;
+
+use magma_app::module::Module;
+use magma_winit::WinitModule;
+
+pub struct DefaultModules;
+
+impl Module for DefaultModules {
+    fn setup(&self, app: &mut magma_app::App) {
+        app.add_module(WinitModule);
+        app.add_module(UIModule);
+    }
+}
 
 #[cfg(test)]
 mod tests {}
