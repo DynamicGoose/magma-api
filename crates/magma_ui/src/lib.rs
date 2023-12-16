0e1a059ffbf62f9e4b6eb5e5cfcd0137efd389f4
DynamicGoose <gezaahs@gmail.com>
2023-12-16 12:37:15 +0100

    `magma_ui`

--- crates/magma_ui/src/lib.rs
+++ crates/magma_ui/src/lib.rs
@@ -1,20 +1,27 @@
-use magma_app::{module::Module, World};
-pub mod ui;
+//! This crate provides the [`UIModule`], which can be added to your application along with an `update_ui`-system, which you must implement yourself.
+//! For ease of use the crate also reexports [`iced`] and [`iced_winit`].
+//! The [`UIModule`] also adds the [`WinitModule`] to your [`App`](magma_app::App), so you can draw your application to a window.
+//! Usage example using [`iced`]:
+//! ```
+//!
+//! ```
 
-pub struct UIModule;
+use magma_app::{module::Module, World};
+use magma_winit::WinitModule;
+
+pub use iced;
+pub use iced_winit;
+
+pub struct UIModule {
+    pub update_ui: &'static dyn Fn(&mut World),
+}
 
 impl Module for UIModule {
     fn setup(&self, app: &mut magma_app::App) {
+        app.add_module(WinitModule);
         app.add_systems(
             magma_app::SystemType::Update,
-            (vec![], vec![&init_ui, &update_ui]),
-        )
+            (vec![], vec![self.update_ui]),
+        );
     }
-}
-
-fn init_ui(_world: &mut World) {
-    todo!()
-}
-fn update_ui(_: &mut World) {
-    todo!()
 }
--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,6 @@ pub use magma_ui;
 pub use magma_winit;
 
 use magma_app::module::Module;
-use magma_ui::UIModule;
 use magma_winit::WinitModule;
 
 pub struct DefaultModules;
@@ -14,7 +13,6 @@ pub struct DefaultModules;
 impl Module for DefaultModules {
     fn setup(&self, app: &mut magma_app::App) {
         app.add_module(WinitModule);
-        app.add_module(UIModule);
         app.add_module(AudioModule);
     }
 }
