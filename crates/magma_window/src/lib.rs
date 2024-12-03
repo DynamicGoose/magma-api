c656462b574920bc0dc49c122929bf5c151b4440
Géza Ahsendorf <gezaahs@gmail.com>
2024-12-03 15:35:12 +0100

    implement basic Window component

--- crates/magma_window/src/lib.rs
+++ crates/magma_window/src/lib.rs
@@ -1,14 +1,35 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use magma_app::module::Module;
+
+pub use window::Window;
+
+mod window;
+
+pub struct WindowModule;
+
+impl Module for WindowModule {
+    fn setup(self, app: &mut magma_app::App) {
+        app.world.register_component::<Window>();
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use magma_app::App;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn module_setup() {
+        let mut app = App::new();
+        app.add_module(WindowModule);
+        app.world.create_entity((Window::default(),)).unwrap();
+        app.world
+            .query()
+            .with_component::<Window>()
+            .unwrap()
+            .run(|entities| {
+                entities[0]
+                    .component_mut(|window: &mut Window| window.maximized = true)
+                    .unwrap();
+            });
     }
 }
